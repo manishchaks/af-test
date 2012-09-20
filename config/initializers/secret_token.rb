9e35d6cbadea09517fd60e7b30fa512bbb911b38
Manish Chakravarty <manishchaks@gmail.com>
2012-09-20 20:01:54 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AfTest::Application.config.secret_token = 'dc5193d412d8cbea45bef6854ba4c42103ab44f9e2b07e42ed98f387e52f70bf59c50d55a22e350e5e190b08c71807ddf9c763dcffcafbd8df1dee9447f35c31'
